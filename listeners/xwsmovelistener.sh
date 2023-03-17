f3881b6d9bb20c0c395bc54d298124ce46b65469
vvhg1 <victor.hobe.gelting@gmail.com>
2023-03-17 08:00:08 +0100

    [#] added switch to next window and updatearray all write with flock

--- listeners/xwsmovelistener.sh
+++ listeners/xwsmovelistener.sh
@@ -3,7 +3,7 @@
 # xwsmovelistener.sh for xwsmgr
 # This script listens for window movement events and writes them to the xwsmgr fifo
 # This script is called by xwsmgr.sh
-
+fifo="/tmp/xwsmgr_fifo"
 previous_windows_list=$(wmctrl -lG | grep -v " -1 " | awk '{print $1, $2, $3, $4, $5, $6}')
 
 while true; do
@@ -12,11 +12,11 @@ while true; do
         # Calculate the difference between the previous and new window list
         new_windows=$(comm -23 <(echo "$new_windows_list" | sort) <(echo "$previous_windows_list" | sort))
         while read -r line; do
-            echo "window_moved: $line" >/tmp/xwsmgr_fifo
+            flock $fifo echo "window_moved: $line" >$fifo
         done <<<"$new_windows"
         # Update the previous window list
         previous_windows_list=$new_windows_list
     fi
     sleep 0.1
 done
-echo "move_listener_exit" >/tmp/xwsmgr_fifo
+flock $fifo echo "move_listener_exit" >$fifo
